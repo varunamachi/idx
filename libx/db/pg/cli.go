7f9fd5a3c47b9e3918584248889764ac91c996e9
Varuna Amachi <varunamachi@gmail.com>
2022-01-09 19:07:43 +0530

    CLI helper for Postgres

--- libx/db/pg/cli.go
+++ libx/db/pg/cli.go
@@ -1,40 +1,90 @@
 package pg
 
-// func requirePostgres(ctx *cli.Context) (err error) {
-// 	defer teak.LogErrorX("t.pg", "Failed to initialize postgres", err)
+import (
+	"github.com/urfave/cli/v2"
+)
 
-// 	ag := teak.NewArgGetter(ctx)
-// 	var opts ConnOpts
+func WithPostgresFlag(cmd *cli.Command) *cli.Command {
+	cmd.Flags = append(cmd.Flags,
+		&cli.StringFlag{
+			Name: "pg-url",
+			Usage: "URL of the host running postgres, this has the highest" +
+				" precidence",
+			EnvVars:  []string{"PG_URL"},
+			Required: false,
+		},
+		&cli.StringFlag{
+			Name:     "pg-host",
+			Value:    "localhost",
+			Usage:    "Address of the host running postgres",
+			EnvVars:  []string{"PG_HOST"},
+			Required: false,
+		},
+		&cli.IntFlag{
+			Name:     "pg-port",
+			Value:    5432,
+			Usage:    "Port on which postgres is listening",
+			EnvVars:  []string{"PG_PORT"},
+			Required: false,
+		},
+		&cli.StringFlag{
+			Name:     "pg-db",
+			Value:    "",
+			Usage:    "Database name",
+			EnvVars:  []string{"PG_DB"},
+			Required: false,
+		},
+		&cli.StringFlag{
+			Name:     "pg-user",
+			Usage:    "Postgres user name",
+			EnvVars:  []string{"PG_USER"},
+			Required: false,
+		},
+		&cli.StringFlag{
+			Name:     "pg-pass",
+			Value:    "",
+			Usage:    "Postgres password for connection",
+			EnvVars:  []string{"PG_PASS"},
+			Required: false,
+		},
+	)
 
-// 	if !teak.GetConfig("postgresOpts", &opts) {
-// 		opts.Host = ag.GetRequiredString("pg-host")
-// 		opts.Port = ag.GetRequiredInt("pg-port")
-// 		opts.User = ag.GetRequiredString("pg-user")
-// 		opts.DBName = ag.GetRequiredString("pg-db")
-// 		opts.Password = ag.GetRequiredSecret("pg-pass")
-// 	} else {
-// 		teak.Info("t.pg", "Read postgresql options from app config")
+	if cmd.Before == nil {
+		cmd.Before = requirePostgres
+	} else {
+		otherBefore := cmd.Before
+		cmd.Before = func(ctx *cli.Context) (err error) {
+			err = requirePostgres(ctx)
+			if err == nil {
+				err = otherBefore(ctx)
+			}
+			return err
+		}
+	}
 
-// 		// If an option is provided in commandline, it overrides the config
-// 		// parameter
-// 		opts.Host = ag.GetStringOr("pg-host", opts.Host)
-// 		opts.Port = ag.GetIntOr("pg-port", opts.Port)
-// 		opts.User = ag.GetStringOr("pg-user", opts.User)
-// 		opts.DBName = ag.GetStringOr("pg-db", opts.DBName)
-// 		opts.Password = ag.GetSecretOr("pg-pass", opts.Password)
-// 	}
-// 	db, err := ConnectWithOpts(&opts)
-// 	if err != nil {
-// 		err = teak.LogErrorX("t.pg", "Failed to open postgres connection", err)
-// 		return err
-// 	}
-// 	err = db.Ping()
-// 	if err != nil {
-// 		err = teak.LogErrorX("t.pg", "Failed to ping postgres DB", err)
-// 		return err
-// 	}
-// 	SetDefaultConn(db)
-// 	teak.Info("t.pg", "Connected to postgres server at %s:%d - to DB: %s",
-// 		opts.Host, opts.Port, opts.DBName)
-// 	return err
-// }
+	return cmd
+}
+
+func requirePostgres(ctx *cli.Context) error {
+	url := ctx.String("pg-url")
+	if url != "" {
+		db, err := Connect(url)
+		if err != nil {
+			return err
+		}
+		SetDefaultConn(db)
+	} else {
+		db, err := ConnectWithOpts(&ConnOpts{
+			Host:     ctx.String("pg-host"),
+			Port:     ctx.Int("pg-port"),
+			User:     ctx.String("pg-user"),
+			DBName:   ctx.String("pg-db"),
+			Password: ctx.String("pg-pass"),
+		})
+		if err != nil {
+			return err
+		}
+		SetDefaultConn(db)
+	}
+	return nil
+}
