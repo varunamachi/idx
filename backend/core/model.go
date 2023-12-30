d396a2f60c33d4141205dfd5fcdb8d5913b5fa7e
varunamachi <varunamachi@gmail.com>
2023-12-30 19:16:34 +0530

    Minor changes

--- backend/core/model.go
+++ backend/core/model.go
@@ -23,16 +23,17 @@ var Disabled UserState = "disabled"
 var Flagged UserState = "flagged"
 
 type User struct {
-	ID         uint64    `json:"id" db:"id"`
-	UserId     string    `json:"userId" db:"user_id"`
-	EmailId    string    `json:"email" db:"email"`
-	Auth       auth.Role `json:"auth" db:"auth"`
-	FirstName  string    `json:"firstName" db:"first_name"`
-	LastName   string    `json:"lastName" db:"last_name"`
-	Title      string    `json:"title" db:"title"`
-	CreatedAt  time.Time `json:"createdAt" db:"created_at"`
-	ModifiedAt time.Time `json:"modifiedAt" db:"modified_at"`
-	Props      data.M    `json:"props,omitempty" db:"props"`
+	ID         uint64             `json:"id" db:"id"`
+	UserId     string             `json:"userId" db:"user_id"`
+	EmailId    string             `json:"email" db:"email"`
+	Auth       auth.Role          `json:"auth" db:"auth"`
+	FirstName  string             `json:"firstName" db:"first_name"`
+	LastName   string             `json:"lastName" db:"last_name"`
+	Title      string             `json:"title" db:"title"`
+	CreatedAt  time.Time          `json:"createdAt" db:"created_at"`
+	ModifiedAt time.Time          `json:"modifiedAt" db:"modified_at"`
+	Props      data.M             `json:"props,omitempty" db:"props"`
+	Perms      auth.PermissionSet `json:"perms,omitempty" db:"perms"`
 }
 
 func (u *User) SeqId() int {
@@ -77,7 +78,8 @@ func (u *User) Prop(key string) any {
 type UserStorage interface {
 	Save(user *User) (err error)
 	Update(user *User) (err error)
-	Get(id string) (user *User, err error)
+	GetOne(id string) (user *User, err error)
 	SetState(id string, state UserState) error
 	Remove(id string) error
+	Get(params data.CommonParams) ([]*User, error)
 }
