da526d6e9915e6b3233600a9a8d3ca4e75375c8f
varunamachi <varunamachi@gmail.com>
2024-02-02 22:31:03 +0530

    WIP - adding user rest apis

---
+++ backend/core/security.go
@@ -0,0 +1,8 @@
+package core
+
+const (
+	PermCreateUser   = "idx.createUser"
+	PermDeleteUser   = "idx.deleteUser"
+	PermGetUser      = "idx.getUser"
+	PermSetUserState = "idx.setUserState"
+)
--- backend/restapi/user.go
+++ backend/restapi/user.go
@@ -3,33 +3,84 @@ package restapi
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/varunamachi/idx/core"
+	"github.com/varunamachi/libx/errx"
 	"github.com/varunamachi/libx/httpx"
 )
 
 func UserEndpoints(us core.UserStorage) []*httpx.Endpoint {
-	return []*httpx.Endpoint{}
-}
-
-func createUserEp(us core.UserStorage) echo.HandlerFunc {
-	return func(etx echo.Context) error {
-		return nil
+	return []*httpx.Endpoint{
+		createUserEp(us),
 	}
 }
 
-func updateUserEp(us core.UserStorage) echo.HandlerFunc {
-	return func(etx echo.Context) error {
+func createUserEp(us core.UserStorage) *httpx.Endpoint {
+	handler := func(etx echo.Context) error {
+		var user core.User
+		if err := etx.Bind(&user); err != nil {
+			return errx.BadReq("failed to read user info from request", err)
+		}
+
+		if err := us.Save(etx.Request().Context(), &user); err != nil {
+			return err
+		}
 		return nil
+	}
+
+	return &httpx.Endpoint{
+		Method:     echo.POST,
+		Path:       "/user",
+		Category:   "idx.user",
+		Desc:       "Create a user",
+		Version:    "v1",
+		Permission: core.PermCreateUser,
+		Handler:    handler,
 	}
 }
 
-func getUser(us core.UserStorage) echo.HandlerFunc {
-	return func(etx echo.Context) error {
+func updateUserEp(us core.UserStorage) *httpx.Endpoint {
+	handler := func(etx echo.Context) error {
 		return nil
+	}
+
+	return &httpx.Endpoint{
+		Method:     echo.POST,
+		Path:       "/user",
+		Category:   "idx.user",
+		Desc:       "Create a user",
+		Version:    "v1",
+		Permission: core.PermCreateUser,
+		Handler:    handler,
 	}
 }
 
-func getUsers(us core.UserStorage) echo.HandlerFunc {
-	return func(c echo.Context) error {
+func getUser(us core.UserStorage) *httpx.Endpoint {
+	handler := func(etx echo.Context) error {
 		return nil
+	}
+
+	return &httpx.Endpoint{
+		Method:     echo.POST,
+		Path:       "/user",
+		Category:   "idx.user",
+		Desc:       "Create a user",
+		Version:    "v1",
+		Permission: core.PermCreateUser,
+		Handler:    handler,
+	}
+}
+
+func getUsers(us core.UserStorage) *httpx.Endpoint {
+	handler := func(c echo.Context) error {
+		return nil
+	}
+
+	return &httpx.Endpoint{
+		Method:     echo.POST,
+		Path:       "/user",
+		Category:   "idx.user",
+		Desc:       "Create a user",
+		Version:    "v1",
+		Permission: core.PermCreateUser,
+		Handler:    handler,
 	}
 }
