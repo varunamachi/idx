77ad2b3c4df328bd0446c6f817d7bd8ff04336b4
varunamachi <varunamachi@gmail.com>
2024-02-06 22:38:58 +0530

    WIP - adding serve command

--- backend/cmd/serve.go
+++ backend/cmd/serve.go
@@ -1,1 +1,29 @@
 package cmd
+
+import (
+	"github.com/rs/zerolog/log"
+	"github.com/urfave/cli/v2"
+	"github.com/varunamachi/libx"
+	"github.com/varunamachi/libx/data/pg"
+)
+
+func serveCommand() *cli.Command {
+	return pg.Wrap(&cli.Command{
+		Name:        "serve",
+		Usage:       "Start the idx identity service",
+		Description: "Start the idx identity service",
+		Action: func(ctx *cli.Context) error {
+
+			gtx := ctx.Context
+			app := libx.MustGetApp(ctx)
+
+			go func() {
+				<-gtx.Done()
+				log.Info().Msg("stopping the server")
+				app.StopServer()
+			}()
+
+			return nil
+		},
+	})
+}
