69d0bd3fc460b4fb8336657f90bb43e8b385fd13
varunamachi <varunamachi@gmail.com>
2024-02-07 21:43:36 +0530

    Added serve command

--- backend/cmd/idx/main.go
+++ backend/cmd/idx/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/rs/zerolog/log"
+	"github.com/varunamachi/idx/cmd"
 	idxpg "github.com/varunamachi/idx/pg"
 	"github.com/varunamachi/idx/restapi"
 	"github.com/varunamachi/libx"
@@ -28,7 +29,8 @@ func main() {
 	app.WithServer(8080, userGetter).
 		WithEndpoints(restapi.UserEndpoints(userStore)...).
 		WithEndpoints(restapi.GroupEndpoints(groupStore)...).
-		WithEndpoints(restapi.ServiceEndpoints(serviceStore)...)
+		WithEndpoints(restapi.ServiceEndpoints(serviceStore)...).
+		WithCommands(cmd.ServeCommand())
 
 	if err := app.RunContext(gtx, os.Args); err != nil {
 		log.Fatal().Err(err).Msg("exiting...")
--- backend/cmd/serve.go
+++ backend/cmd/serve.go
@@ -1,17 +1,26 @@
 package cmd
 
 import (
+	"net/http"
+
 	"github.com/rs/zerolog/log"
 	"github.com/urfave/cli/v2"
 	"github.com/varunamachi/libx"
 	"github.com/varunamachi/libx/data/pg"
 )
 
-func serveCommand() *cli.Command {
+func ServeCommand() *cli.Command {
 	return pg.Wrap(&cli.Command{
 		Name:        "serve",
 		Usage:       "Start the idx identity service",
 		Description: "Start the idx identity service",
+		Flags: []cli.Flag{
+			&cli.UintFlag{
+				Name:  "port",
+				Value: 8888,
+				Usage: "Port at which the service has to run",
+			},
+		},
 		Action: func(ctx *cli.Context) error {
 
 			gtx := ctx.Context
@@ -22,6 +31,12 @@ func serveCommand() *cli.Command {
 				log.Info().Msg("stopping the server")
 				app.StopServer()
 			}()
+
+			if err := app.Serve(uint32(ctx.Uint("port"))); err != nil {
+				if err != http.ErrServerClosed {
+					return err
+				}
+			}
 
 			return nil
 		},
